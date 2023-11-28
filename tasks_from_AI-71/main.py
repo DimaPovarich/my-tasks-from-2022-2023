7ba1235e23ca03d3be750636fc7dfbc396175fbd
DimaPovarich <131833894+DimaPovarich@users.noreply.github.com>
2023-11-28 09:40:03 +0700

    task from class in AI-71
    this code is searching for 'x' in 'txt' file 'exl1'

--- tasks_from_AI-71/main.py
+++ tasks_from_AI-71/main.py
@@ -1,7 +1,11 @@
 f = open('exl1')
-countAB, countAC, s = 0, 0, f.read()
+s = f.read()
+count = 1
 for i in range(len(s)-1):
-    if s[i] == 'A':
-        if s[i+1] == 'B': countAB += 1
-        else: countAC += 1
-print(f"количество AB: {countAB}, количество AC: {countAC}")
+    r = s.find('x')
+    if s[r+1] == 'y':
+        if s[r+2] == 'z':
+            count += 2
+        else:
+           count += 1
+print(count)
